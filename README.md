b7722561a50b77a152d07d9bf1ba20990f7fede4
david <maldonadopereira@gmail.com>
2022-06-17 22:55:54 -0300

    - Configuração decouple

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # curso_django
+[![codecov](https://codecov.io/gh/maldonadopereira/curso_django/branch/main/graph/badge.svg?token=RZKWC8R4SN)](https://codecov.io/gh/maldonadopereira/curso_django)
--- pypro/settings.py
+++ pypro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--d%mmen6@$avrwd99g$w1eu40&%dx1o1jz68y7qo^jb)lq)i&6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
