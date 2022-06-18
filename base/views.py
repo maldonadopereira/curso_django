0118d6dd736a92d223dc7d9c5a96d43c1aff3257
david <maldonadopereira@gmail.com>
2022-06-17 21:47:11 -0300

    - Código melhorado e Confgurada integração contínua

--- base/views.py
+++ base/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
-from  django.http import HttpResponse
+from django.http import HttpResponse
+
 
 def home(request):
     return HttpResponse('Olá Django')
-
-
-# Create your views here.
