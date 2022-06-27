2424a1195aa843e05ebff4b0f1a5902d36a2d005
david <maldonadopereira@gmail.com>
2022-06-27 19:33:32 -0300

    Adicionado vídeo aperitivo
    close #30

--- pypro/settings.py
+++ pypro/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'collectfast',
     'django.contrib.staticfiles',
     'base',
+    'aperitivos',
 ]
 
 MIDDLEWARE = [
--- pypro/urls.py
+++ pypro/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('aperitivos/', include('aperitivos.urls')),
 
 ]
 
