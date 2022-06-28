d80c9af3c8538e9233cad2703b1f90129de294eb
david <maldonadopereira@gmail.com>
2022-06-27 21:10:34 -0300

    Criar segundo vídeo aperitivo
    close #32

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo', 'vimeo_id': 'chRPSvKwagU'},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 'Zbe2mcmi1MQ'}
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
