310987df2e7f7486530ed1eab1157482d962e484
david <maldonadopereira@gmail.com>
2022-06-27 22:05:31 -0300

    Criada página de Lista de Vídeos Aperitivos
    close #34

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Vídeo Aperitivo', 'vimeo_id': 'chRPSvKwagU'},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 'Zbe2mcmi1MQ'}
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Vídeo Aperitivo', 'vimeo_id': 'chRPSvKwagU'},
-        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 'Zbe2mcmi1MQ'}
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
